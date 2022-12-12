c514e9aab2ec8541bf02cd5227fe7fe03569d616
Hamzah Rushdan <hamzahrushdan79@gmail.com>
2022-12-12 11:11:12 -0500

    commit tree c97d8f5c5838987aba9a64105e7b4e26526694ba

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+var multiply = (num1 * num2);
+multiply(num1 * num2);
+
+let random = 1;
+   random ++;
+
+//function random() {
+   // return Math.random();
+//}
+//onsole.log(random);
+let num3 = 12
+let num4 = 3
+var mod = (num3 % num4);
